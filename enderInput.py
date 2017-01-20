9c32d13c89660dcecc2aefa9ade07e2ca0707a72
AgentEnder <agentendercake@gmail.com>
2017-01-19 22:22:09 -0500

    Original Version
    Module as created initially, after first python lab in Intro to CS

---
+++ enderInput.py
@@ -0,0 +1,44 @@
+#Custom Input Module
+#Written by Craigory Coppola
+#Last updated on September 8,2016
+import sys
+DEFAULT_NUM = "Please enter a number:"
+def getInt(min_value = 0, max_value = False, prompt = DEFAULT_NUM): #min_value defaults to 0, max is not required
+	while True: #continue asking until and integer is given
+		try:
+			x = int(input(prompt + ' '))
+			if(max_value != False):
+				if x>max_value: #Check against max_value
+					raise ValueError('')
+			if x < min_value:
+				raise ValueError(''); #Check against min_value
+			break
+		except ValueError: #Catch any value errors
+			if(max_value != False):
+				print("Please give an integer betweeen %d and %d!" %(min_value,max_value))
+			else:
+				print("Please give an integer greater than %d!" %(min_value))
+	return x
+
+def getFloat(min_value = 0, max_value = False, prompt = DEFAULT_NUM):
+	while True: #continue asking until a valid value is given
+		try:
+			x = float(input(prompt + ' '))
+			if(max_value != False):
+				if x>max_value: #Check against max_value
+					raise ValueError('')
+			if x < min_value:
+				raise ValueError(''); #Check against min_value
+			break
+		except ValueError:
+			if(max_value != False):
+				print("Please give a number betweeen %d and %d!" %(min_value,max_value))
+			else:
+				print("Please give a number greater than %d!" %(min_value))
+	return x
+def getString(prompt="Input a string:"):
+	x = input(prompt + " ")
+	if x == "exit":
+		sys.exit()
+	else:
+		return x
